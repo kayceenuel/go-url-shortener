ea801674c3096b496f8462b0a24304cf42770f3d
Kaycee Nuel <heykayceee@gmail.com>
2025-01-16 18:10:26 +0100

    setting up server

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/api/shorten", handleShorten)
-	http.HandleFunc("/", handleRedirect)
+	http.HandleFunc("/shorten", handleshorten)
+	http.HandleFunc("", handleRedirect)
 
 	log.Println("Server starting on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleshorten(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func handleShorten(w http.ResponseWriter, r *http.Request) {}
+func handleRedirect(w http.ResponseWriter, r *http.Request) {
 
-func handleRedirect(w http.ResponseWriter, r *http.Request) {}
+}
